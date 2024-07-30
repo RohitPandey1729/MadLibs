2df97e94e0c00d984f519baa12d755468ff1362e
Rohit Pandey <myboxrohit@gmail.com>
2024-07-30 11:26:45 -0700

    I added more files to make a more complicated Madlib

---
+++ sample_madlibs/hungergames.py
@@ -0,0 +1,18 @@
+def madlib():
+    number = input("Number: ")
+    adj = input("Adjective: ")
+    verb = input("Verb: ")
+    verb2 = input("Verb: ")
+    noun = input("Noun: ")
+    noun2 = input("Noun: ")
+    noun3 = input("Noun: ")
+    noun4 = input("Noun: ")
+    noun5 = input("Noun: ")
+    noun_plural = input("Noun (plural): ")
+    noun_plural_2 = input("Noun (plural): ")
+    noun_plural_3 = input("Noun (plural): ")
+    sound_important = input("Name of something that sounds important: ")
+
+    madlib=f"{number} seconds. That’s how long we’re required to {verb} on our metal circles before \
+the sound of a {noun} releases us. Step off before the {number} seconds is up, and {noun_plural} blow your \
+legs off. {number} seconds to take in the ring of tributes all equidistant from the {sound_important}, a giant \
