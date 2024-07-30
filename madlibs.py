51c95c255edf898bc331bad943f0e2685848a580
Rohit Pandey <myboxrohit@gmail.com>
2024-07-30 13:03:21 -0700

    I reverted back to my original code and finished my short project

--- madlibs.py
+++ madlibs.py
@@ -1,4 +1,15 @@
+"""
+Very Beginner Python Project by Kylie Ying
+Madlibs using string concatenation
 
+YouTube Kylie Ying: https://www.youtube.com/ycubed 
+Twitch KylieYing: https://www.twitch.tv/kylieying 
+Twitter @kylieyying: https://twitter.com/kylieyying 
+Instagram @kylieyying: https://www.instagram.com/kylieyying/ 
+Website: https://www.kylieying.com
+Github: https://www.github.com/kying18 
+Programmer Beast Mode Spotify playlist: https://open.spotify.com/playlist/4Akns5EUb3gzmlXIdsJkPs?si=qGc4ubKRRYmPHAJAIrCxVQ 
+"""
 # # string concatenation (aka how to put strings together)
 # # suppose we want to create a string that says "subscribe to _____ "
 # youtuber = "Kylie Ying" # some string variable
@@ -12,7 +23,6 @@ adj = input("Adjective: ")
 verb1 = input("Verb: ")
 verb2 = input("Verb: ")
 famous_person = input("Famous person: ")
-
 
 madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
 I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
--- random_madlibs.py
+++ random_madlibs.py
@@ -1,6 +1,6 @@
-from sample_madlibs import hp, code, zombie, hungergames
+from sample_madlibs import  code,  hungergames
 import random
 
 if __name__ == "__main__":
-    m = random.choice([hp, code, zombie, hungergames])
+    m = random.choice([ code,  hungergames])
     m.madlib()
